53240f36691e932565e90756e156ecb77485a25d
linyuqing <yqlin@stl>
2020-08-21 15:17:07 +0800

    ssr pac analysis

--- scripts/pac/pac-base64.py
+++ scripts/pac/pac-base64.py
@@ -2,6 +2,10 @@ import base64
 
 if __name__ == '__main__':
     with open(r"./gfwlist.txt", "rb") as gfwlist:
-        gfwlist_text = base64.b64decode(gfwlist.read())
-        print(gfwlist_text)
+        b_wlist = base64.b64decode(gfwlist.read())
+        wlist_text =  b_wlist.decode('utf-8')
+        print(wlist_text)
         gfwlist.close()
+        with open(r"./fwlist.md", "w") as fwlist:
+            fwlist.write(wlist_text)
+            fwlist.close()
