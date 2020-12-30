ee7daa98a62e6b00070ad0fc36358f8a509f628e
linyuqing <yqlin@stl>
2020-12-30 10:11:29 +0800

    feat: 添加新内容

--- code/lrncpp/consoleApp/consoleApp.cpp
+++ code/lrncpp/consoleApp/consoleApp.cpp
@@ -1,33 +1,43 @@
-// consoleApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// consoleApp.cpp : This file contains the 'main' function. Program execution
+// begins and ends there.
 //
 
 #include <iostream>
+#include <map>
 
-#include "udp_client.h"
-#include "regedit_test.h"
 #include "logging.h"
+#include "regedit_test.h"
+#include "udp_client.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+  std::cout << "Hello World!\n";
 
-    UDPClient local_client = UDPClient();
-    if(local_client.InitClient())
-        local_client.Run();
+  // UDPClient local_client = UDPClient();
+  // if(local_client.InitClient())
+  //    local_client.Run();
 
-    //Logging thisLog = Logging();
-    //thisLog.InitLogging();
+  // Logging thisLog = Logging();
+  // thisLog.InitLogging();
 
-    //RegeditTest::TestMain();
+  // RegeditTest::TestMain();
+
+  std::map<std::string, int> api_info = {{"base", 3}, {"friend", 3}};
+
+  std::cout << api_info["base"] << std::endl;
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   5. Go to Project > Add New Item to create new code files, or Project > Add
+//   Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project
+//   and select the .sln file
