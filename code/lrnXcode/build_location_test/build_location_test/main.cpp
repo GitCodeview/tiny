010847606b7c257f1c199bcf7fcd6d757039bd35
LT <LT@STL>
2021-01-18 10:40:52 +0800

    feat(test): xcode cpp build location test

---
+++ code/lrnXcode/build_location_test/build_location_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  build_location_test
+//
+//  Created by stl on 2020/12/11.
+//  Copyright © 2020 stl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
